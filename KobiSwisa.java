ba468f22d05167d1335553b258523f5b3ddb1545
KobiRingelSwisa <kobyswisa@gmail.com>
2024-08-12 15:46:58 +0300

    updated the KobiSwisa.java file

--- KobiSwisa.java
+++ KobiSwisa.java
@@ -1,7 +1,7 @@
 public class KobiSwisa{
 	public static void main(String[] args) {
 		for (int i=1 ; i<=7 ; i++) {
-		System.out.println("Kobi Swisa - print no. "+i);
+		System.out.println("Kobi Swisa - countdown - print no. "+i);
 		}
 	}
 }
